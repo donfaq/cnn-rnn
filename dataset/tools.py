5e410ce754fd4a468496ae2b88abcf38d56cf23c
Gregory Zhigalov <zhigalov8@gmail.com>
2017-03-25 00:17:50 +0300

    Improved frames groupping

--- dataset/tools.py
+++ dataset/tools.py
@@ -2,11 +2,20 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def split_video_frames(filename, step):
-    vid = cv2.VideoCapture(filename)
-    hasNext, image = vid.read()
-    element = []
+def group(data, batch=3, step=1):
+    start = 0
+    end = batch
     result = []
+    while end <= len(data):
+        result.append(data[start:end])
+        start += step
+        end += step
+    return result
+
+
+def video_to_frames(filename, step):
+    vid = cv2.VideoCapture(filename)
+    elements = []
     count = 0
     hasNext = True
     while hasNext:
@@ -14,14 +23,14 @@ def split_video_frames(filename, step):
         if not hasNext:
             break
         count += 1
-        element.append(tuple((count, image)))
-        if count % step == 0:
-            result.append(element)
-            element = []
-    return result
+        elements.append(tuple((count, image)))
+    return elements
 
 
 if __name__ == '__main__':
-    frames = split_video_frames("11_2_3.avi", 2)
-    plt.imshow(frames[1][1][1], cmap='gray', interpolation='bicubic')
+    data = group(video_to_frames("11_2_3.avi", 2))
+    print(data[0][1][1] is data[1][0][1])
+    # True [[[1, img1], [2,img2], [3,img3], [[2,img2], [..], [..]]]
+
+    plt.imshow(data[1][1][1], cmap='gray', interpolation='bicubic')
     plt.show()
