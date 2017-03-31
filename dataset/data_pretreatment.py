138bb681d96cba2f33b920d90f2fbcbc854557a6
Gregory Zhigalov <zhigalov8@gmail.com>
2017-03-31 20:44:35 +0300

    Brand new data preprocessing
    Frames class for video processing and frame groupping and decoding.
    Dataset class for generating and writing (just writing yet) .tfrecords files from dataset videos.

---
+++ dataset/data_pretreatment.py
@@ -0,0 +1,97 @@
+import cv2
+import tensorflow as tf
+
+
+class Frames:
+    videofile_name = ''
+    frames = []
+    grouped_frames = []
+    grouped_frames_decoded = []
+    group_numbers_sequence = []
+    frames_number_in_group = []
+    number_of_frames = int
+    number_of_grouped_frames = int
+
+    def __init__(self, videofile_name, group_length=3, step=1):
+        self.videofile_name = videofile_name
+        self.number_of_frames = 0
+        self.video_to_frames()
+        self.group_frames(group_length=group_length, step=step)
+        self.decode_frames()
+
+    def video_to_frames(self):
+        videofile = cv2.VideoCapture(self.videofile_name)
+        readSucceded = True
+        while readSucceded:
+            readSucceded, frame = videofile.read()
+            if not readSucceded:
+                break
+            self.frames.append(frame)
+        assert len(self.frames) is not 0
+        self.number_of_frames = len(self.frames)
+
+    def group_frames(self, group_length, step):
+        current_frame_group = 0
+        start_group_index = 0
+        end_group_index = group_length
+        while end_group_index <= self.number_of_frames:
+            number_in_group = 0
+            for frame in self.frames[start_group_index:end_group_index]:
+                self.grouped_frames.append(frame)
+                self.group_numbers_sequence.append(current_frame_group)
+                self.frames_number_in_group.append(number_in_group)
+                number_in_group += 1
+            start_group_index += step
+            end_group_index += step
+            current_frame_group += 1
+        self.number_of_grouped_frames = len(self.grouped_frames)
+
+    def decode_frames(self):
+        with tf.Session():
+            for frame in self.grouped_frames:
+                self.grouped_frames_decoded.append(
+                    tf.image.encode_jpeg(frame, format='rgb', quality=100).eval())
+
+
+class Dataset:
+    VIDEOS_PATH_PATTERN = ''
+    videofiles_names_list = []
+
+    def __init__(self, input_path):
+        self.VIDEOS_PATH_PATTERN = input_path
+        self.videofiles_names_list = tf.gfile.Glob(input_path)
+
+    def create_dataset(self, group_length=3, step=1):
+        for vname in self.videofiles_names_list:
+            with open('{vname}.tfrecord'.format(vname=vname), 'w') as tfrecord:
+                writer = tf.python_io.TFRecordWriter(tfrecord.name)
+                frames = Frames(vname, group_length=group_length, step=step)
+                example = self.make_sequence_example(frames)
+                writer.write(example.SerializeToString())
+                writer.close()
+            tfrecord.close()
+
+    @staticmethod
+    def make_sequence_example(frames):
+        example_sequence = tf.train.SequenceExample()
+        example_sequence.context.feature["length"].int64_list.value.append(frames.number_of_grouped_frames)
+        frames_sequence = example_sequence.feature_lists.feature_list["frames"]
+        groups = example_sequence.feature_lists.feature_list["groups"]
+        number_in_group = example_sequence.feature_lists.feature_list["number_in_group"]
+        for frame, group, number in zip(frames.grouped_frames_decoded, frames.group_numbers_sequence,
+                                        frames.frames_number_in_group):
+            if frame is not None:
+                frames_sequence.feature.add().bytes_list.value.append(frame)
+            if group is not None:
+                groups.feature.add().int64_list.value.append(group)
+            if number is not None:
+                number_in_group.feature.add().int64_list.value.append(number)
+        return example_sequence
+
+
+if __name__ == '__main__':
+    # frames_class = Frames('src/1.avi')
+    # print(len(frames_class.grouped_frames_decoded))
+    dataset = Dataset('src/*.avi')
+    dataset.create_dataset()
+    print(dataset.videofiles_names_list)
