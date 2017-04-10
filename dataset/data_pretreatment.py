bba112e41656ca6bd9f7f46d64168b9a07442778
Gregory Zhigalov <zhigalov8@gmail.com>
2017-04-11 01:03:07 +0300

    Dataset batches in tfrecords

--- dataset/data_pretreatment.py
+++ dataset/data_pretreatment.py
@@ -57,9 +57,6 @@ class Frames:
 
 
 class Dataset:
-    VIDEOS_PATH_PATTERN = ''
-    videofiles_names_list = []
-
     def __init__(self, input_path=str()):
         self.VIDEOS_PATH_PATTERN = input_path
         self.videofiles_names_list = tf.gfile.Glob(input_path)
@@ -74,7 +71,7 @@ class Dataset:
         label = int(name.split('_')[-1])
         return label, extention
 
-    def create_dataset(self, group_length=3, step=1):
+    def create_dataset(self, batch_size=30, group_length=3, step=1):
         with tf.Session() as sess:
             for vname in self.videofiles_names_list:
                 print('\n' + vname)
@@ -85,43 +82,71 @@ class Dataset:
                     print('obtaining frames')
                     frames = Frames(sess, vname, group_length=group_length, step=step)
                     print('creating example')
-                    example = self.make_sequence_example(frames, label, extention)
-                    print('writing example')
-                    writer.write(example.SerializeToString())
+                    for example in self.make_examples_list(frames, label, extention, batch_size):
+                        print('writing example')
+                        writer.write(example.SerializeToString())
                     print('done writing')
                     writer.close()
-                    del writer, frames, example
+                    del writer, frames
                 tfrecord.close()
         sess.close()
         del sess
 
     @staticmethod
-    def make_sequence_example(frames, label, extention):
-        example_sequence = tf.train.SequenceExample()
+    def make_example(frames_list, groups_list, numbers_list, label, extention, length):
+        example = tf.train.SequenceExample()
         # context features
-        example_sequence.context.feature["length"].int64_list.value.append(frames.number_of_grouped_frames)
-        example_sequence.context.feature["label"].int64_list.value.append(label)
-        example_sequence.context.feature["extention"].bytes_list.value.append(extention)
+        example.context.feature["length"].int64_list.value.append(length)
+        example.context.feature["label"].int64_list.value.append(label)
+        example.context.feature["extention"].bytes_list.value.append(extention)
         # sequence features
-        frames_sequence = example_sequence.feature_lists.feature_list["frame"]
-        groups = example_sequence.feature_lists.feature_list["group"]
-        number_in_group = example_sequence.feature_lists.feature_list["number_in_group"]
+        frames_sequence = example.feature_lists.feature_list["frame"]
+        groups = example.feature_lists.feature_list["group"]
+        number_in_group = example.feature_lists.feature_list["number_in_group"]
         # 'fill' sequence features
+        for frame, group, number in zip(frames_list, groups_list, numbers_list):
+            frames_sequence.feature.add().bytes_list.value.append(frame)
+            groups.feature.add().int64_list.value.append(group)
+            number_in_group.feature.add().int64_list.value.append(number)
+        return example
+
+    @staticmethod
+    def make_examples_list(frames: Frames, label, extention, batch_size) -> list:
+        batch_counter = 0
+        tmp_frames = []
+        tmp_groups = []
+        tmp_numbers = []
+        examples_list = []
         for frame, group, number in zip(frames.grouped_frames_decoded, frames.group_numbers_sequence,
                                         frames.frames_number_in_group):
-            if frame is not None:
-                frames_sequence.feature.add().bytes_list.value.append(frame)
-            if group is not None:
-                groups.feature.add().int64_list.value.append(group)
-            if number is not None:
-                number_in_group.feature.add().int64_list.value.append(number)
-        return example_sequence
+            if batch_counter < batch_size:
+                if frame is not None:
+                    tmp_frames.append(frame)
+                if group is not None:
+                    tmp_groups.append(group)
+                if number is not None:
+                    tmp_numbers.append(number)
+                batch_counter += 1
+            else:
+                examples_list.append(Dataset.make_example(tmp_frames, tmp_groups, tmp_numbers,
+                                                          label, extention, len(tmp_frames)))
+                batch_counter = 0
+                tmp_frames = []
+                tmp_groups = []
+                tmp_numbers = []
+                if frame is not None:
+                    tmp_frames.append(frame)
+                if group is not None:
+                    tmp_groups.append(group)
+                if number is not None:
+                    tmp_numbers.append(number)
+                batch_counter += 1
+        return examples_list
 
     @staticmethod
     def read(filename_queue):
-
-        reader = tf.TFRecordReader()
-        _, serialized_example = reader.read(filename_queue)
+        # reader = tf.TFRecordReader()
+        # _, serialized_example = reader.read(filename_queue)
 
         context_features = {
             "length": tf.FixedLenFeature([], dtype=tf.int64),
@@ -134,33 +159,38 @@ class Dataset:
             "group": tf.FixedLenSequenceFeature([], dtype=tf.int64),
             "number_in_group": tf.FixedLenSequenceFeature([], dtype=tf.int64),
         }
+        context_data, sequence_data = [], []
+        for serialized_example in tf.python_io.tf_record_iterator(filename_queue):
+            context_parsed, sequence_parsed = tf.parse_single_sequence_example(
+                serialized=serialized_example,
+                context_features=context_features,
+                sequence_features=sequence_features
+            )
+            context_data.append(context_parsed)
+            sequence_data.append(sequence_parsed)
 
-        context_parsed, sequence_parsed = tf.parse_single_sequence_example(
-            serialized=serialized_example,
-            context_features=context_features,
-            sequence_features=sequence_features
-        )
-
-        return context_parsed, sequence_parsed
+        return context_data, sequence_data
 
 
 if __name__ == '__main__':
-    queue = tf.train.string_input_producer(['SDHA2010Interaction/segmented_set1/0_1_4.avi.tfrecord'])
-    context, sequence = Dataset().read(queue)
-    my_img = tf.image.decode_jpeg(sequence['frame'][1], channels=3)
-    init_op = tf.global_variables_initializer()
+    # dataset = Dataset('SDHA2010Interaction/segmented_set2/*0.avi')
+    # dataset.create_dataset()
+
+    # only strings not FIFOQueue
+    context, sequence = Dataset().read('SDHA2010Interaction/segmented_set1/4_1_0.avi.tfrecord')
+    from pprint import pprint
+    pprint(sequence)
+    my_img = tf.image.decode_jpeg(sequence[9]['frame'][29], channels=3)
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
     with tf.Session() as sess:
         sess.run(init_op)
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
-
         image = my_img.eval()
-        print(image.shape)
 
         RGB_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         Image.fromarray(RGB_img, 'RGB').save('tmp.jpg')
         coord.request_stop()
         coord.join(threads)
-
