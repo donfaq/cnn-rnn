ebc4fc7f5dad3d8d3e12c0a9575fc35d3c3d8321
Gregory Zhigalov <zhigalov8@gmail.com>
2017-04-22 20:17:21 +0300

    Trying to use tf.slim

--- network/train.py
+++ network/train.py
@@ -1,42 +1,77 @@
 import tensorflow as tf
 import numpy as np
-
+import tensorflow.contrib.slim as slim
 from dataset.dataset import read_tfrecord, batch_size, height, width
 
-x = tf.placeholder(dtype=tf.float32, shape=(batch_size, height, width, 3))  # (2, 240, 320, 3)
-y = tf.placeholder(dtype=tf.int32, shape=1)
+restore = False
+logdir = 'network/train_logs'
+chkpt_dir = 'network/chkpt'
+learning_rate = 0.001
 
-with tf.name_scope("conv1"):
-    conv1_w = tf.Variable(tf.truncated_normal_initializer(mean=0.0, stddev=0.05, dtype=tf.float32)(shape=(5, 5, 3, 16)),
+with tf.Graph().as_default():
+    x = tf.placeholder(dtype=tf.float32, shape=(batch_size, height, width, 3))  # (2, 240, 320, 3)
+    y = tf.placeholder(dtype=tf.int32, shape=1)
+
+    with slim.arg_scope([slim.conv2d], stride=1):
+        with tf.variable_scope('BlockA', [x]):
+            conv1 = slim.conv2d(x, 32, [1, 1], stride=2, scope='conv1')
+            conv2 = slim.conv2d(conv1, 32, [3, 3], scope='Conv2')
+            pool = slim.max_pool2d(conv2, [3, 3], scope='Pool', stride=1)
+            conv3 = slim.conv2d(pool, 32, [3, 3], stride=2, scope='Conv3')
+
+    size = np.prod(conv3.get_shape().as_list()[1:])  # 307200
+
+    # rnn cell
+    rnn_inputs = tf.reshape(conv3, (-1, batch_size, size))
+    cell = tf.contrib.rnn.GRUCell(100)
+    init_state = cell.zero_state(1, dtype=tf.float32)
+    rnn_outputs, _ = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state=init_state)
+    output = tf.reduce_mean(rnn_outputs, axis=1)
+
+    # full-connected
+    dense_w = tf.Variable(tf.truncated_normal_initializer(mean=0.0, stddev=0.05, dtype=tf.float32)(shape=(100, 6)),
                           tf.float32, name="w")
-    conv1_b = tf.Variable(tf.zeros(shape=(16)), name="b")
-    conv1 = tf.nn.relu(tf.nn.conv2d(x, conv1_w, strides=[1, 2, 2, 1], padding="SAME") + conv1_b,
-                       name="conv")  # (2, 120, 160, 16)
+    dense_b = tf.Variable(tf.zeros([6]), tf.float32, name="b")
+    score = tf.add(tf.matmul(output, dense_w), dense_b, name="out")
 
-size = np.prod(conv1.get_shape().as_list()[1:]) # 307200
+    prediction = tf.cast(tf.arg_max(score, dimension=1), tf.int32)
+    loss = slim.losses.sparse_softmax_cross_entropy(logits=score, labels=y)
 
-rnn_inputs = tf.reshape(conv1, (-1, batch_size, size))
-cell = tf.contrib.rnn.GRUCell(100)
-init_state = cell.zero_state(1, dtype=tf.float32)
+    total_loss = slim.losses.get_total_loss()
 
-rnn_outputs, _ = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state=init_state)
-output = tf.reduce_mean(rnn_outputs, axis=1)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 
-dense_w = tf.Variable(tf.truncated_normal_initializer(mean=0.0, stddev=0.05, dtype=tf.float32)(shape=(100, 6)),
-                      tf.float32, name="w")
-dense_b = tf.Variable(tf.zeros([6]), tf.float32, name="b")
-score = tf.add(tf.matmul(output, dense_w), dense_b, name="out")
+    global_step = tf.Variable(0, name='global_step', trainable=False)  # learning rate
 
-prediction = tf.cast(tf.arg_max(score, dimension=1), tf.int32)
-loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=score, labels=y)
+    train_op = slim.learning.create_train_op(total_loss, optimizer)
+    train = slim.learning.train(train_op,
+                                logdir,
+                                number_of_steps=1000,
+                                save_summaries_secs=300,
+                                save_interval_secs=600)
 
 
-with tf.Session() as sess:
-    for video in tf.gfile.Glob("dataset/SDHA2010Interaction/segmented_set2/*.tfr"):
-        label, batches = read_tfrecord(video)
-        for batch in batches:
-            sess.run(tf.global_variables_initializer())
-            feed_dict = {x: batch, y: label}
-            R_loss, R_prediction = sess.run([loss, prediction], feed_dict=feed_dict)
-            print("Loss: ", R_loss)
-            print("Prediction: ", R_prediction)
+if __name__ == '__main__':
+    print("Net0: ", conv1)
+    print("Net: ", conv2)
+    print("Pool: ", pool)
+    print("Conv1: ", conv3)
+    print("RNN inputs ", rnn_inputs)
+    print("RNN outputs: ", rnn_outputs)
+    print("Output: ", output)
+    with tf.Session() as sess:
+
+        # if restore:
+        #     restorer.restore(sess, chkpt_dir + "/data.chkp")
+        #     print("model restored")
+
+        for video in tf.gfile.Glob("dataset/SDHA2010Interaction/segmented_set2/?_11_?.tfr"):
+            label, batches = read_tfrecord(video)
+            for batch in batches:
+                feed_dict = {x: batch, y: label}
+                sess.run(slim.variables.global_variables_initializer())
+                op = sess.run([train])
+                # _, l = sess.run([train, loss], feed_dict=feed_dict)
+
+                print(op)
+                # saver.save(sess, chkpt_dir + "/data.chkp")
