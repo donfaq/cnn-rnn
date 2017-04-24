cdd92f906eeea814aa25f878603efabc9b909f2c
Gregory Zhigalov <zhigalov8@gmail.com>
2017-04-25 00:56:01 +0300

    Learning by random examples (30frames)

---
+++ dataset/Reader.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tensorflow as tf
+import os
+
+from dataset.dataset import batch_size, height, width
+
+zip_options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)
+
+
+class Reader:
+    def __init__(self, data_pattern):
+        self.data_pattern = data_pattern
+        self.files = np.array(tf.gfile.Glob(self.data_pattern))
+        self.init_dataset()
+
+    def get_next_example_group(self):
+        label, video = self.read_tfrecord(str(self.iterator.value))
+        self.iterator.iternext()
+        return self.iterator.finished, label, video
+
+    def get_random_example_group(self, length):
+        r_labels, r_examples = [], []
+        for i in range(length):
+            label, video = self.read_tfrecord(np.random.choice(self.files))
+            r_labels.append(label)
+            r_examples.append(video[np.random.choice(video.shape[0], 1, replace=False)][0])
+        return np.array(r_labels).reshape(length, 1), np.array(r_examples)
+
+    def get_random_example(self):
+        label, video = self.read_tfrecord(np.random.choice(self.files))
+        return np.array(label), video[np.random.choice(video.shape[0], 1, replace=False)][0]
+
+    def read_tfrecord(self, path):
+        batches = []
+        filename, label = self.parse_tfr_filename(path)
+        for string_record in tf.python_io.tf_record_iterator(path=filename, options=zip_options):
+            example = tf.train.Example()
+            example.ParseFromString(string_record)
+            img_string = (example.features.feature['batch'].bytes_list.value[0])
+            data = np.fromstring(img_string, dtype=np.uint8).reshape((batch_size, height, width, -1))
+            batches.append(data)
+        return label, np.array(batches)
+
+    def parse_tfr_filename(self, path):
+        filename, ext = os.path.splitext(path)
+        path, file = os.path.split(filename)
+        return "{}/{}.tfr".format(path, file), np.array([int(file.split('_')[-1])])
+
+    def normalize_images(self, data) -> np.ndarray:
+        return data / 255
+
+    def init_dataset(self):
+        np.random.shuffle(self.files)
+        self.iterator = np.nditer(self.files)
--- network/train.py
+++ network/train.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import tensorflow.contrib.slim as slim
-from dataset.dataset import read_tfrecord, batch_size, height, width
 
-restore = True
+from dataset.Reader import Reader
+from dataset.dataset import batch_size, height, width
+
+pattern = "dataset/SDHA2010Interaction/segmented_set1/*.tfr"
+restore = False
 is_training = True
 logs_path = 'network/logs'
 chkpt_file = logs_path + "/model.ckpt"
@@ -12,27 +15,27 @@ group_size = 6
 classes_num = 6
 
 with tf.Graph().as_default() as graph:
-    x = tf.placeholder(dtype=tf.float32, shape=(None, batch_size, height, width, 3), name="input")  # (2, 240, 320, 3)
-    y = tf.placeholder(dtype=tf.int32, shape=(None, 1), name='labels')
-    flatten_x = tf.reshape(x, (-1, height, width, 3))
+    x = tf.placeholder(dtype=tf.float32, shape=(batch_size, height, width, 3), name="input")  # (2, 240, 320, 3)
+    y = tf.placeholder(dtype=tf.int32, shape=(1,), name='labels')
+    # flatten_x = tf.reshape(x, (-1, height, width, 3))
 
     with slim.arg_scope([slim.conv2d], stride=1):
-        with tf.variable_scope('Convolution', [flatten_x]):
-            conv1 = slim.conv2d(flatten_x, 32, [1, 1], stride=2, scope='Conv1')
-            dropout = slim.dropout(conv1, 0.8, is_training=is_training)
-            pool2 = slim.max_pool2d(dropout, [3, 3], scope='Pool1', stride=1)
+        with tf.variable_scope('Convolution', [x]):
+            conv1 = slim.conv2d(x, 32, [1, 1], stride=2, scope='Conv1')
+            # dropout = slim.dropout(conv1, 0.8, is_training=is_training)
+            pool2 = slim.max_pool2d(conv1, [3, 3], scope='Pool1', stride=1)
             conv2 = slim.conv2d(pool2, 32, [3, 3], scope='Conv2')
-            dropout = slim.dropout(conv2, 0.8, is_training=is_training)
-            pool3 = slim.max_pool2d(dropout, [3, 3], scope='Pool2', stride=1)
-            conv3 = slim.conv2d(pool3, 32, [3, 3], stride=2, scope='Conv3')
-            net = slim.dropout(conv3, 0.7, is_training=is_training, scope='Pool3')
+            # dropout = slim.dropout(conv2, 0.8, is_training=is_training)
+            pool3 = slim.max_pool2d(conv2, [3, 3], scope='Pool2', stride=1)
+            net = slim.conv2d(pool3, 32, [3, 3], stride=2, scope='Conv3')
+            # net = slim.dropout(conv3, 0.7, is_training=is_training, scope='Pool3')
 
     size = np.prod(net.get_shape().as_list()[1:])  # 307200
 
-    with tf.variable_scope('GRU_RNN_cell', [flatten_x]):
+    with tf.variable_scope('GRU_RNN_cell'):
         rnn_inputs = tf.reshape(net, (-1, batch_size, size))
         cell = tf.contrib.rnn.GRUCell(100)
-        init_state = cell.zero_state(tf.shape(x)[0], dtype=tf.float32)
+        init_state = cell.zero_state(1, dtype=tf.float32)
         rnn_outputs, _ = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state=init_state)
         output = tf.reduce_mean(rnn_outputs, axis=1)
 
@@ -51,7 +54,7 @@ with tf.Graph().as_default() as graph:
         tf.summary.scalar("accuracy", accuracy)
     with tf.name_scope('Cost'):
         cross_entropy = slim.losses.sparse_softmax_cross_entropy(logits=logits, labels=y, scope='cross_entropy')
-        tf.summary.scalar("cost", cross_entropy)
+        tf.summary.scalar("cross_entropy", cross_entropy)
     with tf.name_scope('Optimizer'):
         global_step = tf.Variable(0, name='global_step', trainable=False)
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss=cross_entropy,
@@ -60,38 +63,23 @@ with tf.Graph().as_default() as graph:
     saver = tf.train.Saver()
 
 if __name__ == '__main__':
-
-    print("X: ", x)
-    print("FlattenX: ", flatten_x)
-    print("conv1: ", conv1)
-    print("dropout: ", dropout)
-    print("conv2: ", conv2)
-    print("pool2: ", pool2)
-    print("Conv1: ", net)
-    print("RNN inputs ", rnn_inputs)
-    print("RNN outputs: ", rnn_outputs)
-    print("Output: ", output)
-    print("Logits: ", logits)
-
     with tf.Session(graph=graph) as sess:
+
         if restore:
             saver.restore(sess, chkpt_file)
             print("Model restored.")
+        else:
+            sess.run(tf.global_variables_initializer())
 
-        # sess.run(slim.variables.global_variables_initializer())
         writer = tf.summary.FileWriter(logs_path, graph=graph)
+        reader = Reader(pattern)
 
         while True:
-
-            for video in tf.gfile.Glob("dataset/SDHA2010Interaction/segmented_set1/?_1_?.tfr"):
-                label, video_frames = read_tfrecord(video)
-                for group in np.array_split(video_frames, group_size):
-                    label_arr = np.array([label] * len(group))
-
-                    feed_dict = {x: group, y: label_arr}
-                    _, summary, acc, gs = sess.run([train_step, summary_op, accuracy, global_step], feed_dict=feed_dict)
-                    writer.add_summary(summary, gs)
-                    print("Num epochs {} - Accuracy: {}".format(gs, acc))
-                    if gs % 10:
-                        save_path = saver.save(sess, chkpt_file)
-                        print("Model saved in file: %s" % save_path)
+            label, example = reader.get_random_example()
+            feed_dict = {x: example, y: label}
+            _, summary, acc, gs = sess.run([train_step, summary_op, accuracy, global_step], feed_dict=feed_dict)
+            writer.add_summary(summary, gs)
+            print("Global step {} - Accuracy: {}".format(gs, acc))
+            if gs % 100 == 0:
+                save_path = saver.save(sess, chkpt_file)
+                print("Model saved in file: %s" % save_path)
